6150c1a11fb4dfb4c8f2adda4b6819adfa0ce34d
BigMing <532692737@qq.com>
2017-09-12 10:42:57 +0800

    update

---
+++ src/leetcodeTest/test410_splitArray.java
@@ -0,0 +1,55 @@
+package leetcodeTest;
+
+public class test410_splitArray {
+	/**
+	 * 把数组分成m个部分，使得每个部分最大的和最小
+	 * @param nums
+	 * @param m
+	 * @return
+	 */
+	public static int splitArray(int[] nums, int m) {
+		// 第一步，设置左右界限
+		long R = 1; // 上界是全部的和，右边是开区间
+		for (int i = 0; i < nums.length; i++) {
+			R += nums[i];
+		}
+		long L = 0; // 下界是0
+		long ans = 0;
+		// 第二步
+		while (L < R) {
+			long mid = (L + R) / 2;
+			if (guess(mid, nums, m)) {
+				ans = mid;
+//				L = mid + 1;
+				R = mid;
+			} else {
+//				R = mid;
+				L = mid + 1;
+			}
+		}
+		return (int) ans;
+	}
+
+	/**
+	 * 
+	 * @param mid 数组的部分和
+	 * @param nums 数组
+	 * @param m 需要分成m个部分
+	 * @return 当和最大值为mid的时候，此时是否合格
+	 */
+	private static boolean guess(long mid, int[] nums, int m) {
+		long sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (sum + nums[i] > mid) {
+				m--; // 一个部分的和超过mid了
+				sum = nums[i]; // 新开辟一个部分，nums[i]是第一个值
+				if (nums[i] > mid) {
+					return false;
+				}
+			} else {
+				sum += nums[i];
+			}
+		}
+		return m >= 1; // 留一组给sum用
+	}
+}
---
+++ src/leetcodeTest/test69_mySqrt.java
@@ -0,0 +1,37 @@
+package leetcodeTest;
+
+/**
+ * 二分法求答案的过程就是猜答案和答案之间的关系，然后不断逼近
+ * 求根号的反函数转换成求平方，再转换成不等式问题x^2 <= y
+ * @author sjm
+ */
+public class test69_mySqrt {
+	/**
+	 * 判断x的平方是否小于y
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public static boolean guess(long x, long y) {
+		return x * x <= y; // 这里x的平方可能会越界，所以转换成long型
+	}
+	
+	public static int mySqrt(int y) {
+		long L = 0, R = (long) y + 1; // 定义左界限和右界限
+		long ans = 0;
+		while (L < R) {
+			long mid = (L + R) / 2;
+			if (guess(mid, y)) {
+				ans = mid; // 记录下答案，防止死循环
+				L = mid + 1; // 注意这里不加1的话，无法跳出循环
+			} else {
+				R = mid;
+			}
+		}
+		return (int) ans;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(mySqrt(10));
+	}
+}
