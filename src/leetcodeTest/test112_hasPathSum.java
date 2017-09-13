06ac3eb18953e18d1c2ca07d70d3240c9fc76787
BigMing <532692737@qq.com>
2017-09-13 21:30:50 +0800

    update

---
+++ src/leetcodeTest/test112_hasPathSum.java
@@ -0,0 +1,41 @@
+package leetcodeTest;
+
+public class test112_hasPathSum {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public boolean hasPathSum(TreeNode root, int sum) {
+		if (root == null) {
+			return false;
+		} else if (root.left == null && root.right == null && root.val == sum) {
+			return true;
+		} else {
+			return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+		}
+	}
+	
+	/**
+	 * 前序遍历，深度优先
+	 * @param r
+	 * @param want
+	 * @param now
+	 * @return
+	 */
+	public boolean visit(TreeNode r, int want, int now) {
+		if (r == null) {
+			return false;
+		}
+		if (r.left == null && r.right == null) { // 叶节点
+			return (now + r.val == want);
+		} else { // 非叶节点
+			return visit(r.left, want, now + r.val) || visit(r.right, want, now + r.val);
+		}
+	}
+}
---
+++ src/leetcodeTest/test124_maxPathSum.java
@@ -0,0 +1,42 @@
+package leetcodeTest;
+
+public class test124_maxPathSum {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+	int ans;
+	/**
+	 * 求当前节点的最大深度值，前序遍历
+	 * @param r
+	 * @return
+	 */
+	int maxDeep(TreeNode r) {
+		if (r == null) {
+			return 0;
+		}
+		if (r.left == null && r.right == null) {
+			ans = Math.max(ans, r.val);
+			return r.val;
+		} else {
+			int maxLeft = maxDeep(r.left);
+			int maxRight = maxDeep(r.right);
+			int temp = Math.max(maxLeft, maxRight) + r.val;
+			temp = Math.max(temp, r.val); // 子节点的值有可能是负数
+			// 考虑节点有负数的情况
+			ans = Math.max(ans, temp);
+			ans = Math.max(ans, maxLeft + maxRight + r.val);
+			return temp;
+		}
+	}
+	
+	public int maxPathSum(TreeNode root) {
+		ans = Integer.MIN_VALUE;
+        maxDeep(root);
+        return ans;
+    }
+}
