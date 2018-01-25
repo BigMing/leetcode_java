fa97b73866c4011b758c20f659e1c7a533f31aa5
BigMing <532692737@qq.com>
2018-01-25 13:55:32 +0800

    update

---
+++ src/leetcodeTest/test102_BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,46 @@
+package leetcodeTest;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class test102_BinaryTreeLevelOrderTraversal {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		
+		Queue<TreeNode> q = new LinkedList<TreeNode>();
+
+		if (root == null)
+			return res;
+
+		q.offer(root);
+		int mark = 0;
+		while (!q.isEmpty()) {
+			mark = q.size(); // 用mark记录一层是个数
+			List<Integer> tmpList = new ArrayList<Integer>();
+			TreeNode tmpNode = new TreeNode(0);
+			
+			for (int i = 0; i < mark; i++) {
+				tmpNode = q.poll();
+				tmpList.add(tmpNode.val);
+				if (tmpNode.left != null)
+					q.offer(tmpNode.left);
+				if (tmpNode.right != null)
+					q.offer(tmpNode.right);
+			}
+			res.add(tmpList);
+		}
+		return res;
+	}
+}
